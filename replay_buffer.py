830344db72989353c1bb06335130c76b707680b4
Jirilin <sjirilin@gmail.com>
2026-07-09 11:13:02 +0100

    Refactor ReplayBuffer: update initialization parameters, enhance add method, and improve sample logic

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,31 +1,31 @@
-
 import random
 import torch
 from collections import deque
 
 class ReplayBuffer:
-    def __init__(self, capacity=500, device='cpu'):
+    def __init__(self, capacity=1000, device="cpu"):
         self.capacity = capacity
         self.device = device
         self.buffer = deque(maxlen=capacity)
-    
+        self.total_added = 0
+        self.total_evicted = 0
+
     def add(self, images, labels):
         for i in range(len(images)):
-            self.buffer.append((images[i].cpu(), labels[i].cpu()))
-    
+            if len(self.buffer) == self.capacity:
+                self.total_evicted += 1
+            self.buffer.append((images[i].detach().cpu(), labels[i].detach().cpu()))
+            self.total_added += 1
+
     def sample(self, batch_size):
-        if len(self.buffer) < batch_size:
-            # If buffer not full enough, return all
-            batch = list(self.buffer)
-        else:
-            batch = random.sample(self.buffer, batch_size)
+        if len(self.buffer) == 0:
+            return None, None
+        batch = list(self.buffer) if len(self.buffer) < batch_size else random.sample(self.buffer, batch_size)
         images, labels = zip(*batch)
-        images = torch.stack(images).to(self.device)
-        labels = torch.tensor(labels).to(self.device)
-        return images, labels
-    
+        return torch.stack(images).to(self.device), torch.tensor(labels, device=self.device)
+
+    def stats(self):
+        return {"size": len(self.buffer), "capacity": self.capacity, "added": self.total_added, "evicted_oldest": self.total_evicted}
+
     def __len__(self):
         return len(self.buffer)
-    
-    def clear(self):
-        self.buffer.clear()
